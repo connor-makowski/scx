6b1883dba2a8e7c43e4776e04f8bd66c8d675291
Connor Makowski <connorman528@gmail.com>
2022-12-05 16:39:56 -0500

    Update Notebooks and Notebook structure

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ MIT's Supply Chain Micromaster (SCx) Python Package
 ## Documentation
 [Technical documentation](https://connor-makowski.github.io/scx/index.html) can be found [here](https://connor-makowski.github.io/scx/index.html).
 
-## Setup
+## Examples
+- [Optimization Examples](https://github.com/connor-makowski/scx/tree/main/notebooks/optimization/)
 
-### Quick Reference Example Notebook (Google Colab)
-- [Example Notebook Here](https://colab.research.google.com/github/connor-makowski/scx/blob/main/notebooks/optimization/P1.ipynb)
+## Setup
 
 ### Cloud Setup (Google Colab)
 - You can access google colab [here](https://colab.research.google.com/)
 - Create a new notebook
-- Install the `scx` package by adding the following to a new cell at the top of your notebook and running it:
+- Install the `scx` package by adding the following to a new code cell at the **top** of your notebook and running it:
   - `pip install scx`
 
 
---
+++ test/optimize/t8.py
@@ -0,0 +1,38 @@
+from scx.optimize import Model
+
+distances=[
+    [105, 256, 86],
+    [240, 136, 198],
+]
+cost_per_mi=0.12
+
+demand=[2500,4350,3296]
+
+assembly_plants=['A1','A2']
+demand_regions=['R1','R2','R3']
+
+decision_variables=[[Model.variable(name=f"{a}__{d}__amt", lowBound=0) for d in demand_regions] for a in assembly_plants]
+
+# Initialize the model
+my_model = Model(name="Blinky22", sense='minimize')
+
+# Add the Objective Fn
+my_model.add_objective(
+    fn=Model.sum([distances[i][j]*decision_variables[i][j]*cost_per_mi for i in range(len(assembly_plants)) for j in range(len(demand_regions))]),
+)
+
+# Add the Constraints
+## Demand Constraint
+for j in range(len(demand_regions)):
+    my_model.add_constraint(
+        name=f"Constraint__{demand_regions[j]}",
+        fn=Model.sum([decision_variables[i][j] for i in range(len(assembly_plants))])>=demand[j],
+    )
+
+try:
+    # Solve my_model
+    my_model.solve()
+    my_model.get_formulation()
+    my_model.get_outputs()
+except:
+    print ("optimize/t8.py failed")
